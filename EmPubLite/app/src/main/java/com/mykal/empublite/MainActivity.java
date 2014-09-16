779b387fc964a33e77500dfd46f40640806de80d
Mykal Cuin <mykcuin@gmail.com>
2014-09-16 13:49:14 -0600

    Working with moving to another activity, and some xml chnages.

--- EmPubLite/app/src/main/java/com/mykal/empublite/MainActivity.java
+++ EmPubLite/app/src/main/java/com/mykal/empublite/MainActivity.java
@@ -1,9 +1,11 @@
 package com.mykal.empublite;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 
 public class MainActivity extends Activity {
@@ -18,19 +20,28 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.options, menu);
+        return (super.onCreateOptionsMenu(menu));
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                return (true);
+
+            case R.id.about:
+                Intent i = new Intent(this, SimpleContentActivity.class);
+                startActivity(i);
+
+                return (true);
+
+            case R.id.help:
+                i = new Intent(this, SimpleContentActivity.class);
+                startActivity(i);
+
+                return (true);
         }
-        return super.onOptionsItemSelected(item);
+        return(super.onOptionsItemSelected(item));
     }
 }
