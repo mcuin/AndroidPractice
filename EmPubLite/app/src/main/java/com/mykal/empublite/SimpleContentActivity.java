7016392c94388f7d1dff7a0230825da2fccac3ab
Mykal Cuin <mcuin@mines.edu>
2014-09-16 13:49:14 -0600

    Working with moving to another activity, and some xml chnages.

---
+++ EmPubLite/app/src/main/java/com/mykal/empublite/SimpleContentActivity.java
@@ -0,0 +1,15 @@
+package com.mykal.empublite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SimpleContentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
