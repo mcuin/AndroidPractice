4f0e96201484018f906befb7218023b45ecc8309
Mykal Cuin <mykcuin@gmail.com>
2015-02-16 15:09:22 -0700

    Android Developers, Adding the Action Bar: Adding Action Buttons

--- ActionBar/app/src/main/java/com/mykal/actionbar/MainActivity.java
+++ ActionBar/app/src/main/java/com/mykal/actionbar/MainActivity.java
@@ -27,13 +27,17 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
