79791f230464803939378b64d053cc2f5ec636c5
Mykal Cuin <mykcuin@gmail.com>
2015-02-19 12:12:31 -0700

    Android Developers, Saving Data: Saving Files

--- SavingData/app/src/main/java/com/mykal/savingdata/MainActivity.java
+++ SavingData/app/src/main/java/com/mykal/savingdata/MainActivity.java
@@ -1,12 +1,69 @@
 package com.mykal.savingdata;
 
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 
 public class MainActivity extends ActionBarActivity {
+
+
+
+
+
+    public void Write(Context context) {
+        String filename = "myfile";
+        String string = "Hello world!";
+        FileOutputStream outputStream;
+        File file = new File(context.getFilesDir(), filename);
+
+        try{
+            outputStream = openFileOutput(filename,Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public File getTempFile(Context context, String url) {
+        File file;
+
+        try {
+            String filename = Uri.parse(url).getLastPathSegment();
+            file = File.createTempFile(filename, null, context.getCacheDir());
+        } catch (Exception e) {
+            //Error
+        }
+        return file;
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public File getAlbumStorageDir(String albumName) {
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                albumName);
+        if (!file.mkdir()) {
+            Log.e(LOG_TAG, "Directory not created.");
+        }
+        return file;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
