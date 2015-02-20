41a15fde42fc7b2208b843fd806005827a602a26
Mykal Cuin <mykcuin@gmail.com>
2015-02-20 10:18:45 -0700

    Android Developers, Interacting with Other Apps: Sending User to Another App

---
+++ InteractingWithOtherApps/app/src/main/java/com/mykal/interactingwithotherapps/MainActivity.java
@@ -0,0 +1,84 @@
+package com.mykal.interactingwithotherapps;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.apache.http.protocol.HTTP;
+
+import java.util.Calendar;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Uri number = Uri.parse("tel:5551234");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+
+        Uri location = Uri.parse("geo:0,0?q=1600+Amphitheater+Parkway,+Mountain+View,+California");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+        Uri webpage = Uri.parse("http://www.android.com");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType(HTTP.PLAIN_TEXT_TYPE);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"jon@example.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email Subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message here");
+        emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"));
+
+        Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+        Calendar beginTime = Calendar.getInstance().set(2012, 0, 19, 7, 30);
+        Calendar endTime = Calendar.getInstance().set(2012, 0, 19, 10, 30);
+        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+        calendarIntent.putExtra(CalendarContract.Events.TITLE, "Ninja Class");
+        calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Secret Dojo");
+
+        PackageManager manager = getPackageManager();
+        List<ResolveInfo> activities = manager.queryIntentActivities(mapIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+
+        String title = getResources().getString(R.string.chooser_title);
+        Intent chooser = Intent.createChooser(mapIntent, title);
+
+        if (isIntentSafe) {
+            startActivity(mapIntent);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
