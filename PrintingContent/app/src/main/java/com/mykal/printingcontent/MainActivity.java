c3b26a88a40970d1460a0fbcb7ae69fbc8b9fd5c
Mykal Cuin <mykcuin@gmail.com>
2015-03-20 11:21:18 -0600

    Android Developers, Printing Content: HTML Documents

--- PrintingContent/app/src/main/java/com/mykal/printingcontent/MainActivity.java
+++ PrintingContent/app/src/main/java/com/mykal/printingcontent/MainActivity.java
@@ -1,16 +1,26 @@
 package com.mykal.printingcontent;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintJob;
+import android.print.PrintManager;
 import android.support.v4.print.PrintHelper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends Activity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,5 +59,40 @@ public class MainActivity extends Activity {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.droids);
 
         photoPrint.printBitmap("droids.jpg - test print", bitmap);
+    }
+
+    private void doWebPrint() {
+        WebView webView = new WebView(getActivity());
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.i(TAG, "page finished loading" + url);
+                createWebPrintJob(view);
+                mWebView = null;
+            }
+        });
+
+        String htmlDocument = "<html><body><h1>Test Content</h1><p>Testing, " + "testing, testing..." +
+                "</p></body></html>";
+
+        webView.loadDataWithBaseURL("file:///android_assets/images/", htmlDocument, "text/HTML", "UTF-8", null);
+
+        mWebView = webView;
+    }
+
+    private void createWebPrintJob(WebView webView) {
+
+        PrintManager manager = (PrintManager) getActivity.getSystemService(Context.PRINT_SERVICE);
+        PrintDocumentAdapter adapter = webView.createPrintDocumentAdapter();
+        String jobName = getString(R.string.app_name) + "Document";
+        PrintJob job = manager.print(jobName, adapter, new PrintAttributes.Builder().build());
+
+        mJob.add(job);
     }
 }
